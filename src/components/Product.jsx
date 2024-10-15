4c14522383a20f036db39fc75d5e6dc0e54c55dd
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-15 22:40:59 +0300

    up

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,14 @@
-// src/App.jsx
+// src/components/Product.jsx
 
 export default function Product() {
+  const price = 999;
+  const imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+
   return (
-    <div>
-      <h2>Cookies</h2>
-	  <p>Price: 999 credits</p>
-    </div>
+    <>
+      <h2>Tacos</h2>
+	  <img src= {imgUrl} alt="Tacos With Lime" width="640" />
+      <p>Price: {price} credits</p>
+    </>
   );
 };
