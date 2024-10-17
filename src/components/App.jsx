1848a44aa306a9beed81d28b3de42f36556d33e2
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-17 13:39:59 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
-// src/components/App.jsx
+import BookList from "./BookList";
+import favouriteBooks from "./data";
+import Product from './Product'
 
-import Product from "./Product";
-
-export default function App() {
+const App = () => {
   return (
     <div>
-      <h1>Best selling</h1>
-
-      <Product
-        name="Tacos With Lime"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
+    <>
+	  <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+    </>
+    <h2>Products</h2>
+      <ul>
+        <Product price={22} imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="640" name='Oculus' />
+        
+        <Product name='Ivan' />
+        
+        <Product name='Dada'/>
+      </ul>
     </div>
   );
-}
+};
+
+export default App
