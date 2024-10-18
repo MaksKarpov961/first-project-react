7681b7b92e96b92d25b6d09078cf3a9f3359363c
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-18 11:22:57 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import BookList from "./BookList";
 import favouriteBooks from "./data";
 import Product from './Product'
+import { Alert } from "./Alert";
 
 const App = () => {
   return (
@@ -17,6 +18,21 @@ const App = () => {
         
         <Product name='Dada'/>
       </ul>
+      <>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+
+      </>
     </div>
   );
 };
