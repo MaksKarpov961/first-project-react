5a0a21e61fb49595de0de05e7719f405a88813dd
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-21 13:25:38 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,26 @@
-import BookList from "./BookList";
-import favouriteBooks from "./data";
-import Product from './Product'
-import { Alert } from "./Alert";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = window.localStorage.getItem("saved-clicks");
+    if (savedClicks !== null) {
+      return Number(savedClicks);
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-clicks", clicks);
+  }, [clicks]);
+
   return (
     <div>
-    <>
-	  <h1>Books of the week</h1>
-      <BookList books={favouriteBooks} />
-    </>
-    <h2>Products</h2>
-      <ul className="product-list">
-        <Product imgUrl="https://s7d1.scene7.com/is/image/mcdonalds/574x400_MIXBOX:3-column-desktop?resmode=sharp2" price={22} name='s' />
-        
-        <Product imgUrl="https://s7d1.scene7.com/is/image/mcdonalds/574x400_NUGGETS:3-column-desktop?resmode=sharp2" price={12} name='Ivan' />
-        
-        <Product imgUrl="https://s7d1.scene7.com/is/image/mcdonalds/UKR_beef_v2:product-header-desktop?wid=1170&hei=643&dpr=off" price={100} name='Dada'/>
-      </ul>
-      <>
-      <Alert outlined='true' variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert elevated='true' variant="error">
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success">
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning">
-        Please update your profile contact information
-      </Alert>
-
-      </>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
     </div>
   );
 };
 
 export default App
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Імпорт стилів нормалізації
+
 import "modern-normalize";
 import App from "./components/App.jsx";
 import "./index.css";
